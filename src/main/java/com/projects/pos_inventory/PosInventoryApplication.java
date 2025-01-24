ba25e4b7a22ffc59a084bfc32a1684a367e8ebbf
Dev Gayashan <tharuka.gayashan.5@gmail.com>
2025-01-24 22:19:46 +0530

    initial commit

---
+++ src/main/java/com/projects/pos_inventory/PosInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.projects.pos_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosInventoryApplication.class, args);
+	}
+
+}
